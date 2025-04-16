5325228ae77cad8ef88138e5968cfbc418884d09
Dinma Faith <phaytep@gmail.com>
2025-04-16 03:32:16 +0100

    Complete styles for faq app

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,5 +1,7 @@
 import React from "react";
 import starIcon from "../assets/images/icon-star.svg";
+import plusIcon from "../assets/images/icon-plus.svg";
+import minusIcon from "../assets/images/icon-minus.svg";
 
 export default function FAQ() {
   return (
@@ -19,49 +21,77 @@ export default function FAQ() {
 
         <div className="faq-content">
           <div className="faq-item">
-            <button className="faq-question">
-              What is Frontend Mentor, and how will it help me?
-            </button>
-            <div className="faq-answer">
+            <div className="faq-btn">
+              <h5 className="faq-question">
+                What is Frontend Mentor, and how will it help me?
+              </h5>
+              <button>
+                <img src={plusIcon} alt="plus icon" />
+                <img src={minusIcon} alt="minus icon" />
+              </button>
+            </div>
+
+            <p className="faq-answer">
               Frontend Mentor offers realistic coding challenges to help
               developers improve their frontend coding skills with projects in
               HTML, CSS, and JavaScript. It's suitable for all levels and ideal
               for portfolio building.
-            </div>
+            </p>
           </div>
 
           {/* FAQ - 2 */}
           <div className="faq-item">
-            <button className="faq-question">Is Frontend Mentor free?</button>
-            <div className="faq-answer">
+            <div className="faq-btn">
+              <h5 className="faq-question">Is Frontend Mentor free?</h5>
+              <button>
+                <img src={plusIcon} alt="plus icon" />
+                <img src={minusIcon} alt="minus icon" />
+              </button>
+            </div>
+
+            <p className="faq-answer">
               Yes, Frontend Mentor offers both free and premium coding
               challenges, with the free option providing access to a range of
               projects suitable for all skill levels.
-            </div>
+            </p>
           </div>
 
           {/* FAQ - 3 */}
           <div className="faq-item">
-            <button className="faq-question">
-              Can I use Frontend Mentor projects in my portfolio?
-            </button>
-            <div className="faq-answer">
+            <div className="faq-btn">
+              <h5 className="faq-question">
+                Can I use Frontend Mentor projects in my portfolio?
+              </h5>
+              <button>
+                <img src={plusIcon} alt="plus icon" />
+                <img src={minusIcon} alt="minus icon" />
+              </button>
+            </div>
+
+            <p className="faq-answer">
               Yes, you can use projects completed on Frontend Mentor in your
               portfolio. It's an excellent way to showcase your skills to
               potential employers!
-            </div>
+            </p>
           </div>
 
           {/* FAQ - 4 */}
-          <div className="faq-item">
-            <button className="faq-question">
-              How can I get help if I'm stuck on a challenge?
-            </button>
-            <div className="faq-answer">
+          <div className=" item">
+            <div className="faq-btn">
+              <h5 className="faq-question">
+                How can I get help if I'm stuck on a challenge?
+              </h5>
+              <button>
+                <img src={plusIcon} alt="plus icon" />
+                <img src={minusIcon} alt="minus icon" />
+              </button>
+            </div>
+
+            <p className="faq-answer">
               The best place to get help is inside Frontend Mentor's Discord
               community. There's a help channel where you can ask questions and
               seek support from other community members.
-            </div>
+            </p>
           </div>
         </div>
       </main>
